98168ea5eab58d8be58db81782719ed5ec94ff43
joelgreen <thejoelgreen@gmail.com>
2017-08-07 15:37:36 -0700

    Initial Commit

---
+++ UberMediaSDKDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UberMediaSDKDemo
+//
+//  Created by Joel Green on 8/7/17.
+//  Copyright © 2017 UberMedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UberMediaSDKDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UberMediaSDKDemo
+//
+//  Created by Joel Green on 8/7/17.
+//  Copyright © 2017 UberMedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
